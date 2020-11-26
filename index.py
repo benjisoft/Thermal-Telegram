dcf8f0bbb46832405056a75fe4f868a46a39ee6e
Benji Lewis <benji@benjisoft.org.uk>
2020-11-26 21:02:44 +0000

    Working remote printing!

--- index.py
+++ index.py
@@ -2,9 +2,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import os
+import threading
+import time
 
 # Use a service account
-cred = credentials.Certificate('path/to/serviceAccount.json')
+cred = credentials.Certificate('serviceAccount.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -14,17 +16,25 @@ callback_done = threading.Event()
 
 # Define printing function
 def tPrint(printing):
-    command = './print.sh ' + printing
+    command = './print.sh ' + str(printing)
     os.system(command)
     return True
 
 # Create a callback on_snapshot function to capture changes
 def on_snapshot(doc_snapshot, changes, read_time):
-    for doc in doc_snapshot:
-        tPrint({doc.data.printing})
+    for change in changes:
+        item = change.document.to_dict()
+        if (change.type.name == "ADDED" and item["printed"] == False):
+            tPrint({item["text"]})
+            db.collection(u'requests').document(change.document.id).set({
+                u'printed': True
+            }, merge=True)
     callback_done.set()
 
 doc_ref = db.collection(u'requests')
 
 # Watch the document
 doc_watch = doc_ref.on_snapshot(on_snapshot)
+
+while True:
+    time.sleep(1)
